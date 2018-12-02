427fef56f4bc699b76d6acfc097a5c5f8b99875e
xandyx2014 <xandyx2014@gmail.com>
2018-12-02 11:20:23 -0400

    basico

---
+++ index.ts
@@ -0,0 +1,13 @@
+import Server from './classes/server';
+import router from './routes/router';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const server = new Server();
+// bodyParser
+server.app.use(cors({origin: true, credentials: true}));
+server.app.use(bodyParser.urlencoded({extended: true}));
+server.app.use(bodyParser.json());
+server.app.use('/', router);
+server.start(() => {
+  console.log(`Servidor corriento en el puerto ${server.port}`);
+});
